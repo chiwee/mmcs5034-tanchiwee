93907919ce8c0eac077fa635dafcf9f0f7269e08
TanCW <76679846+chiwee@users.noreply.github.com>
2025-07-19 11:25:55 +0800

    Add files via upload

---
+++ test99.py
@@ -0,0 +1,19 @@
+#Create a website on your imagination
+# by using the Streamlit CheatSheet 
+import streamlit as st
+
+st.header("Welcome to Crack Detection using Deep Learning")
+st.subheader("brought to you by Sean Tan")
+
+st.audio("Two Steps From Hell - Victory [High quality].mp3")
+st.button("Submit")
+
+st.image("Dashboard.gif")
+
+#st.camera_input("Take a picture")
+
+st.date_input("Select the date")
+
+st.color_picker("Pick a colour")
+
+st.success("Preprocessing completed - 100%")
